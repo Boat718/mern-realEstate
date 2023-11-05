eb401702fd8a725d6717a8bcf371bef2bb0853b2
boat718 <boat_loading@hotmail.com>
2023-11-05 22:47:30 +0700

    connect DB and create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express  from "express";
+import mongoose from "mongoose";
+import connectDB from "./config/dbConfig.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const port = 3000;
 
+connectDB();
+
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+mongoose.connection.once('open',()=>{
+    console.log("Connected to MongoDB");
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+})
