94ced4efbfd200c3650e9d2af9f3cf2880e90dc3
boat718 <boat_loading@hotmail.com>
2023-11-12 21:24:41 +0700

    add search API route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -54,3 +54,50 @@ export const getListing = async(req, res, next) => {
         next(error)
     }
 }
+
+export const getListings = async(req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        let furnished =req.query.furnished;
+        let parking = req.query.parking;
+        let type = req.query.type;
+        let searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        if(offer === undefined || offer === "false") {
+            offer = {$in:[false, true]};
+        }
+
+        if(furnished === undefined || furnished === "false") {
+            furnished = {$in:[false, true]};
+        }
+
+        if(parking === undefined || parking === "false") {
+            parking = {$in:[false, true]};
+        }
+
+        if(type === undefined || type === "false") {
+            type = {$in:['sale', 'rent']};
+        }
+        
+
+        const listing = await Listing.find({
+            name:{$regex: searchTerm, $options:'i'},
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort({
+            [sort]:order
+        }).limit(limit).skip(startIndex);
+        if(!listing) {
+            return next(errorHandler(404, 'Listing not found'));
+        }
+        return res.status(200).json(listing);
+    } catch (error) {
+        next(error)
+    }
+}
